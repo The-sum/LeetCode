e7a2312c02fe09e4904d86962e1627d0d218808a
The_sum <1007183476@qq.com>
2023-01-26 22:41:31 +0800

    1663

---
+++ src/main/java/the/sum/medium/Question_1663.java
@@ -0,0 +1,36 @@
+package the.sum.medium;
+
+import sun.print.SunMinMaxPage;
+
+import java.lang.reflect.Array;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Question_1663 {
+    // 1663. 具有给定数值的最小字符串
+    // n:长度   k:数值
+    // 贪心算法
+    // 假定字符串各个位置都为a，从后往前遍历数组，设置求出数值差d，如果d不小于25直接将最后一位设置为z，如果d小于25
+    // 则将当前位置的字符加上d
+    public static String getSmallestString(int n, int k) {
+        char[] chars = new char[n];
+        Arrays.fill(chars, 'a');
+        int d = k - n;
+        for (int i = n - 1; i >= 0; i--) {
+            if (d >= 25) {
+                chars[i] = 'z';
+                d -= 25;
+            } else if (d > 0) {
+                chars[i] = (char) ('a' + d);
+                break;
+            }
+        }
+        return String.valueOf(chars);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getSmallestString(5, 73));
+    }
+}
